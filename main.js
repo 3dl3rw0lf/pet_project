4358f3d494024ba6c28d1604fac83463ab568391
Gustavo Arauz <arauz.gus@gmail.com>
2024-06-20 18:11:26 -0300

    menu-dev

---
+++ main.js
@@ -0,0 +1,25 @@
+import { menu } from "./script/menu.js";
+
+window.onload = function () {
+	let header = document.getElementById("header");
+	let container = document.createElement("div");
+	container.innerHTML = menu();
+	header.appendChild(container);
+};
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+function toggleHamburgerIcon(hamburger) {
+    hamburger.classList.toggle('open');
+    const divs = hamburger.querySelectorAll('div');
+    if (hamburger.classList.contains('open')) {
+        divs[0].style.transform = 'rotate(45deg) translate(5px, 5px)';
+        divs[1].style.opacity = '0';
+        divs[2].style.transform = 'rotate(-45deg) translate(5px, -5px)';
+    } else {
+        divs[0].style.transform = 'rotate(0) translate(0, 0)';
+        divs[1].style.opacity = '1';
+        divs[2].style.transform = 'rotate(0) translate(0, 0)';
+    }
+}
---
+++ script/link.js
@@ -0,0 +1,20 @@
+export const links = {
+	pages: [
+		{
+			page: "Pichis",
+			link: "/index.html",
+		},
+		{
+			page: "Mascotas Perdidas",
+			link: "/templates/lost_pets.html",
+		},
+		{
+			page: "Mascotas Encontradas",
+			link: "/templates/pets_found.html",
+		},
+		{
+			page: "Encontrar a mi mascota",
+			link: "/templates/contact.html",
+		},
+	],
+};
